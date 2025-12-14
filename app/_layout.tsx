1740116c836a5c1ba0197ce1444efbe65d5724e7
rasult22 <warrior.006@inbox.ru>
2025-12-14 07:01:51 +0500

    Add React Query setup and integrate with RootLayout
    - Introduced QueryClient for managing server state.
    - Updated RootLayout to use QueryClientProvider and StatusBar.
    - Added dependencies for @shopify/flash-list and @tanstack/react-query.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,14 @@
+import { QueryClientProvider } from "@tanstack/react-query";
 import { Stack } from "expo-router";
+import { StatusBar, useColorScheme } from "react-native";
 import "../global.css";
+import queryClient from '../queries/index';
 
 export default function RootLayout() {
-  return <Stack />;
+  const isDarkMode = useColorScheme() === "dark";
+
+  return (<QueryClientProvider client={queryClient}>
+          <StatusBar barStyle={isDarkMode ? "light-content" : "dark-content"} />
+          <Stack />;
+        </QueryClientProvider>);
 }
